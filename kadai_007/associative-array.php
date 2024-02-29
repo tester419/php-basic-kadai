a39a7db304f08c4a970a3a9ed661915f617b6f73
tester419 <139663589+tester419@users.noreply.github.com>
2024-03-01 00:21:49 +0900

    kadai_007をアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <title>kadai_007</title>
+  </head>
+  <body>
+    <p>
+      <?php
+        // 連想配列dataに変数を代入する
+        $data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+        // 連想配列dataの値を出力する
+        print_r($data);
+        
+      ?>
+    </p>
+  </body>
+</html>
